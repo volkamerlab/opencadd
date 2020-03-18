8b100688539e5a8a97dd2bbd07136c30f62b571b
Jaime RGP <jaime.rogue@gmail.com>
2020-03-18 10:48:32 +0100

    Provide base tree for library

---
+++ structuralalignment/superposition/matchmaker.py
@@ -0,0 +1,6 @@
+class MatchMakerAlgorithm(BaseAlignerAlgorithm):
+    def __init__(self, params):
+        self.params = params
+
+    def __call__(self, structure1, structure2, alignment):
+        ...  # TODO: Implement this using the UCSF Chimera algorithm
