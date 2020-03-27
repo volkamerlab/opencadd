08cd42f6a906bfd9603b391e09242947a70aceaf
Jaime RGP <jaime.rogue@gmail.com>
2020-03-27 14:52:06 +0100

    Fix CI issues (maybe)

--- .github/workflows/bash_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-uname -a
-df -h
-ulimit -a
-
-. devtools/scripts/initialize_conda.sh
-conda info
-python devtools/scripts/create_conda_env.py -n=test -p=${{ matrix.python-version }} devtools/conda-envs/test_env.yaml
