40ead6790e84a1867bd27b0b2da5f8b14c5b5848
Jaime RGP <jaime.rogue@gmail.com>
2020-03-31 12:37:09 +0200

    Add some more utilities to the temp dir context manager

--- structuralalignment/utils.py
+++ structuralalignment/utils.py
@@ -5,11 +5,12 @@ import contextlib
 
 
 @contextlib.contextmanager
-def enter_temp_directory():
+def enter_temp_directory(remove=True):
     """Create and enter a temporary directory; used as context manager."""
     temp_dir = tempfile.mkdtemp()
     cwd = os.getcwd()
     os.chdir(temp_dir)
-    yield
+    yield cwd, temp_dir
     os.chdir(cwd)
-    shutil.rmtree(temp_dir)
+    if remove:
+        shutil.rmtree(temp_dir)
