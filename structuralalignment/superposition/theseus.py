948249f2a6982c266ae284874d35836005e0759b
Enes <eneskuni@gmail.com>
2020-04-07 22:57:45 +0200

    cleaned up unified theseus class

--- structuralalignment/superposition/theseus.py
+++ structuralalignment/superposition/theseus.py
@@ -64,49 +64,47 @@ class Theseus(CommandLineWrapper):
         self.alignment_file = "theseus.aln"
         self.alignment_app = "muscle"
 
-    def __create_pdb(self, structures):
+    def _create_pdb(self, structures):
         """
-        Create files with atomium objects
+        Create files with atomium models
 
         Parameter
         ---------
         structures : list
-            list of atomium objects
+            list of atomium models
         """
-        self.fetched_pdbs = structures
-        self.fetched_pdbs_filename = []
+        fetched_pdbs_filename = []
+        for index, pdbs in enumerate(structures):
+            pdb_filename = f"{index}.pdb"
+            pdbs.save(pdb_filename)
+            fetched_pdbs_filename.append(pdb_filename)
+        return fetched_pdbs_filename
 
-        for pdbs in self.fetched_pdbs:
-            pdb_filename = f"{pdbs.code}.pdb"
-            pdbs.model.save(pdb_filename)
-            self.fetched_pdbs_filename.append(pdb_filename)
-        return self.fetched_pdbs_filename
-
-    def _get_fasta(self):
+    def _get_fasta(self, pdbs_filename):
         """
         Use Theseus to create fasta files
 
         .. todo::
             We can probably generate this from atomium directly
         """
-        return subprocess.check_output(["theseus", "-f", "-F", *self.pdbs_filename])
+        return subprocess.check_output(["theseus", "-f", "-F", *pdbs_filename])
 
-    def _concatenate_fasta(self):
+    def _concatenate_fasta(self, pdbs_filename):
         """
         Concatenate the fasta files created with Theseus into one multiple sequence fasta file
         """
         with open(self.fastafile, "w") as outfile:
-            for fname in self.pdbs_filename:
+            for fname in pdbs_filename:
                 with open(f"{fname}.fst") as infile:
                     for line in infile:
                         outfile.write(line)
 
-    def _filemap(self):
+    def _filemap(self, pdbs_filename):
         """
         Create filemap for Theseus
         """
         with open(self.filemap_file, "w") as outfile:
-            for fname in self.pdbs_filename:
+            for fname in pdbs_filename:
                 outfile.write(f"{fname} {fname}\n")
 
     def _calculate(self, structures, identical: bool = True, **kwargs):
@@ -138,34 +136,57 @@ class Theseus(CommandLineWrapper):
         with enter_temp_directory(remove=False) as (cwd, tmpdir):
             print("DEBUG: Running in", tmpdir, " -- TODO: DELETE BEFORE MERGE --")
 
-            self.pdbs_filename = self.__create_pdb(structures)
+            pdbs_filename = self._create_pdb(structures)
 
             if identical:
-                superposition_output = self._run_theseus()
+                superposition_output = self._run_theseus_identical(pdbs_filename)
             else:
-                self._get_fasta()
-                self._concatenate_fasta()
-                self._filemap()
-                seq_alignment_output = subprocess.check_output(
-                    [
-                        self.alignment_app,
-                        "-maxiters",
-                        str(self.max_iterations),
-                        "-in",
-                        self.fastafile,
-                        "-out",
-                        self.alignment_file,
-                        "-clwstrict",
-                    ],
-                    universal_newlines=True,
-                )
-                self._parse_alignment(seq_alignment_output)
-                print(seq_alignment_output)
-                superposition_output = self._run_theseus_align()
+                superposition_output = self._run_theseus_different(pdbs_filename)
                 print(superposition_output)
                 print("-- OUTPUT WE NEED TO PARSE BEFORE DELETING TEMPFILES --")
                 print(*list(Path(tmpdir).glob("theseus_*")), sep="\n")
         return self._parse_superposition(superposition_output)
+
+    def _run_theseus_identical(self, pdbs_filename):
+        """
+        Superpose identical sequences with Theseus
+        """
+        output = subprocess.check_output(["theseus", *pdbs_filename], universal_newlines=True)
+        return output
+
+    def _run_alignment(self):
+        output = subprocess.check_output(
+            [
+                self.alignment_app,
+                "-maxiters",
+                str(self.max_iterations),
+                "-in",
+                self.fastafile,
+                "-out",
+                self.alignment_file,
+                "-clwstrict",
+            ],
+            universal_newlines=True,
+        )
+        return output
+
+    def _run_theseus_different(self, pdbs_filename):
+        """
+        Superpose different sequences with Theseus based on the sequence alignment
+        """
+        self._get_fasta(pdbs_filename)
+        self._concatenate_fasta(pdbs_filename)
+        self._filemap(pdbs_filename)
+        seq_alignment_output = self._run_alignment()
+
+        self._parse_alignment(seq_alignment_output)
+        print(seq_alignment_output)
+
+        output = subprocess.check_output(
+            ["theseus", "-f", "-M", self.filemap_file, "-A", self.alignment_file, *pdbs_filename,],
+            universal_newlines=True,
+        )
+        return output
 
     def _parse_alignment(self, output):
         """
@@ -188,34 +209,8 @@ class Theseus(CommandLineWrapper):
         for line in output.splitlines():
             if "Classical" in line:
                 rmsd = line.split()[5]
-        return {"rmsd": rmsd}
-
-    def _run_theseus(self):
-        """
-        Superpose with Theseus
-        """
-        output = subprocess.check_output(
-            ["theseus", *self.pdbs_filename], universal_newlines=True
-        )
-        return output
-
-    def _run_theseus_align(self):
-        """
-        Superpose with Theseus based on the sequence alignment
-        """
-        output = subprocess.check_output(
-            [
-                "theseus",
-                "-f",
-                "-M",
-                self.filemap_file,
-                "-A",
-                self.alignment_file,
-                *self.pdbs_filename,
-            ],
-            universal_newlines=True,
-        )
-        return output
+        # TODO: return superposed atomium models?
+        return {"rmsd": rmsd, "metadata": {}}
 
 
 if __name__ == "__main__":
@@ -225,9 +220,17 @@ if __name__ == "__main__":
     # pdb_ids = sys.argv[1:3]
     pdb_ids = ["6HG4", "6HG9"]
 
-    models = [atomium.fetch(pdb_id) for pdb_id in pdb_ids]
+    models = [atomium.fetch(pdb_id).model for pdb_id in pdb_ids]
+
     theseus = Theseus()
 
     # theseus.calculate((models))
     theseus._calculate(models, False)
 
+"""
+TODO:
+- Maybe parse more information from output files
+- Try different alignment tool (biotite)
+- Improve documentation
+- Delete debug code
+"""
