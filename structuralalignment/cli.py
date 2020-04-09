a9f509a222e1df1f1a054d3a8af1ee7b7e306c9d
Jaime RGP <jaime.rogue@gmail.com>
2020-04-09 19:08:17 +0200

    Add awesome CLI

--- structuralalignment/cli.py
+++ structuralalignment/cli.py
@@ -5,6 +5,7 @@ Command Line Interface for structuralalignment
 """
 import os
 import argparse
+import logging
 
 import atomium
 
@@ -17,6 +18,13 @@ def parse_cli():
         "structures",
         nargs="+",
         help="PDB IDs or paths to structure files to be aligned. At least two are needed. First one will be considered the target.",
+    )
+    p.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        default=False,
+        help="Whether to print debugging info to stdout",
     )
     p.add_argument(
         "--method",
@@ -47,14 +55,48 @@ def parse_method_options(string):
     return options
 
 
+def greeting():
+    return (
+        """
+   _____ _                   _                   _
+  / ____| |                 | |                 | |
+ | (___ | |_ _ __ _   _  ___| |_ _   _ _ __ __ _| |
+  \___ \| __| '__| | | |/ __| __| | | | '__/ _` | |
+  ____) | |_| |  | |_| | (__| |_| |_| | | | (_| | |
+ |_____/ \__|_|   \__,_|\___|\__|\__,_|_|  \__,_|_|
+     /\   | (_)                                | |
+    /  \  | |_  __ _ _ __  _ __ ___   ___ _ __ | |_
+   / /\ \ | | |/ _` | '_ \| '_ ` _ \ / _ \ '_ \| __|
+  / ____ \| | | (_| | | | | | | | | |  __/ | | | |_
+ /_/    \_\_|_|\__, |_| |_|_| |_| |_|\___|_| |_|\__|
+                __/ |
+               |___/
+
+ v{version} · Brought to you by @volkamerlab
+"""
+    )[1:]
+
+
 def main():
     args = parse_cli()
+    debug_level = logging.INFO if args.verbose else logging.WARNING
+    logging.basicConfig(level=debug_level)
 
-    # Build atomium models (IO)
-    models = []
-    for structure in args.structures:
-        opener = atomium.open if os.path.isfile(structure) else atomium.fetch
-        models.append(opener(structure).model)
+    from ._version import get_versions
 
+    print(greeting().format(version=get_versions()["version"]))
     # Delegate to the API method
-    return align(*models, method=METHODS[args.method], **args.method_options)
+    reference_id, *mobile_ids = args.structures
+
+    opener = atomium.open if os.path.isfile(reference_id) else atomium.fetch
+    reference_model = opener(reference_id).model
+
+    for mobile_id in mobile_ids:
+        opener = atomium.open if os.path.isfile(mobile_id) else atomium.fetch
+        mobile_model = opener(mobile_id).model
+        result, *_empty = align(
+            [reference_model, mobile_model], method=METHODS[args.method], **args.method_options
+        )
+        print(
+            f"RMSD for alignment between `{reference_id}` and `{mobile_id}` is {result['scores']['rmsd']}Å"
+        )
