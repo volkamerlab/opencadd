2610347ba485a964fdbc60b0e44c47e3d92e9c3d
dominiquesydow <dominique.sydow@posteo.de>
2020-08-11 15:01:50 +0200

    Apply @jaimergp's suggestions on api.py

--- opencadd/databases/klifs_new/api.py
+++ opencadd/databases/klifs_new/api.py
@@ -1,7 +1,7 @@
 """
 api.py
 
-Defines API.
+Defines opencadd.databases.klifs API (local and remote).
 """
 
 from bravado.client import SwaggerClient
@@ -17,37 +17,81 @@ KLIFS_CLIENT = SwaggerClient.from_url(
 
 
 class Session:
-    """Class for remote or local session"""
+    """
+    Class to set up remote or local session.
+    Class attributes are None upon initialization but will be updated when remote or local session is called, 
+    using from_remote() or from_local(path_to_klifs_download).
 
-    def __init__(self, path_to_klifs_download=None):
+    Attributes
+    ----------
+    session_type : None or str
+        Session type, i.e. remote or local. 
+    client : None or bravado.client.SwaggerClient
+        KLIFS client (set if session type is remote).
+    database : None or pandas.DataFrame
+        KLIFS metadata (set if session type is local).
+    kinases : None or remote.Kinases or local.Kinases
+        Kinases object for kinases requests.
+    ligands : None or remote.Ligands or local.Ligands
+        Ligands object for ligands requests.
+    structures: None or remote.Structures or local.Structures
+        Structures object for structures requests.
+    bioactivities : None or remote.Bioactivities or local.Bioactivities
+        Bioactivities object for bioactivity requests.
+    interactions : None or remote.Interactions or local.Interactions
+        Interactions object for interactions requests.
+    coordinates : None or remote.Coordinates or local.Coordinates
+        Coordinates object for coordinates requests.
+    """
 
-        if path_to_klifs_download:
-            # Session type
-            self.session_type = "local"
+    def __init__(self):
 
-            # Get database
-            session_initializer = local.SessionInitializer(path_to_klifs_download)
-            self.database = session_initializer.klifs_metadata
+        self.session_type = None
+        self.client = None
+        self.database = None
+        self.kinases = None
+        self.kinases = None
+        self.ligands = None
+        self.structures = None
+        self.bioactivities = None
+        self.interactions = None
+        self.coordinates = None
 
-            # Initialize classes
-            self.kinases = local.Kinases(self.database)
-            self.ligands = local.Ligands(self.database)
-            self.structures = local.Structures(self.database)
-            self.interactions = local.Interactions(self.database)
-            self.coordinates = local.Coordinates(self.database)
+    def from_local(self, path_to_klifs_download):
+        """
+        Set up local session by initializing a local metadata database from KLIFS download.
+        """
 
-        else:
-            # Session type
-            self.session_type = "remote"
+        # Session type
+        self.session_type = "local"
 
-            # Get client
-            self.client = KLIFS_CLIENT
+        # Get database
+        session_initializer = local.SessionInitializer(path_to_klifs_download)
+        self.database = session_initializer.klifs_metadata
 
-            # Initialize classes
-            self.kinases = remote.Kinases(self.client)
-            self.ligands = remote.Ligands(self.client)
-            self.structures = remote.Structures(self.client)
-            self.bioactivities = remote.Bioactivities(self.client)
-            self.interactions = remote.Interactions(self.client)
-            self.coordinates = remote.Coordinates(self.client)
+        # Initialize classes
+        self.kinases = local.Kinases(self.database)
+        self.ligands = local.Ligands(self.database)
+        self.structures = local.Structures(self.database)
+        self.bioactivities = local.Bioactivities(self.client)
+        self.interactions = local.Interactions(self.database)
+        self.coordinates = local.Coordinates(self.database)
 
+    def from_remote(self):
+        """
+        Set up remote session using the KLIFS swagger client.
+        """
+
+        # Session type
+        self.session_type = "remote"
+
+        # Get client
+        self.client = KLIFS_CLIENT
+
+        # Initialize classes
+        self.kinases = remote.Kinases(self.client)
+        self.ligands = remote.Ligands(self.client)
+        self.structures = remote.Structures(self.client)
+        self.bioactivities = remote.Bioactivities(self.client)
+        self.interactions = remote.Interactions(self.client)
+        self.coordinates = remote.Coordinates(self.client)
