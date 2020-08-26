775826bfa9f078b7d1366791863a7096370c005c
Unknown <dominique.sydow@posteo.de>
2020-08-26 15:36:08 +0200

    Add Region class

--- opencadd/structure/subpockets/core.py
+++ opencadd/structure/subpockets/core.py
@@ -165,7 +165,76 @@ class Subpocket:
             return subpocket_center
 
 
-class Residue:
+class Region:
+    """
+    Class defining a region.
+
+    Attributes
+    ----------
+    name : str
+        Region name.
+    color : str
+        Region color.
+    residue_pdb_ids : list of (int, str)
+        List of residue PDB IDs defining the region.
+    residue_labels : list of (int, str)
+        List of residue labels.
+    """
+
+    def __init__(self):
+
+        self.name = None
+        self.color = None
+        self.residue_pdb_ids = None
+        self.residue_labels = None
+
+    def from_dataframe(self, dataframe, name, color, residue_pdb_ids, residue_labels=None):
+        """
+
+        Set region properties.
+        
+        Parameters
+        ----------
+        dataframe : pandas.DataFrame
+            Structural data with the following mandatory columns:
+            "residue.pdb_id", "atom.name", "atom.x", "atom.y", "atom.z"
+        name : str
+            Region name.
+        color : str
+            Region color.
+        residue_pdb_ids : list of (int, str)
+            List of residue PDB IDs defining the region.
+        residue_labels : list of (int, str)
+            List of residue labels.
+        """
+
+        self.name = name
+        self.color = color
+
+        if not residue_labels:
+            residue_labels = [None] * len(residue_pdb_ids)
+
+        if len(residue_pdb_ids) != len(residue_labels):
+            raise ValueError(f"Number of residue PDB IDs and labels must be of same length.")
+
+        # Cast residue PDB IDs and labels to strings
+        residue_pdb_ids = [str(residue_pdb_id) for residue_pdb_id in residue_pdb_ids]
+        residue_labels = [str(residue_label) for residue_label in residue_labels]
+
+        # Add residue labels to dataframe
+        residue_labels_df = pd.DataFrame(
+            {"residue.pdb_id": residue_pdb_ids, "residue.label": residue_labels}
+        )
+        dataframe = dataframe.merge(residue_labels_df, on="residue.pdb_id", how="left")
+
+        # Keep only existing residue PDB IDs
+        residues = dataframe[["residue.pdb_id", "residue.label"]].drop_duplicates()
+        residues.reset_index(drop=True, inplace=True)
+        residues = residues[residues["residue.pdb_id"].isin(residue_pdb_ids)]
+        self.residue_pdb_ids = residues["residue.pdb_id"].to_list()
+        self.residue_labels = residues["residue.label"].to_list()
+
+
 class AnchorResidue:
     """
     Class defining an anchor residue.
