949e1350b757e748fff124017031648a395d9193
Unknown <dominique.sydow@posteo.de>
2020-08-28 14:47:13 +0200

    Do not cast None to str

--- opencadd/structure/subpockets/core.py
+++ opencadd/structure/subpockets/core.py
@@ -58,9 +58,15 @@ class Base:
         if len(residue_pdb_ids) != len(residue_labels):
             raise ValueError(f"Number of residue PDB IDs and labels must be of same length.")
 
-        # Cast residue PDB IDs and labels to strings
-        residue_pdb_ids = [str(residue_pdb_id) for residue_pdb_id in residue_pdb_ids]
-        residue_labels = [str(residue_label) for residue_label in residue_labels]
+        # Cast residue PDB IDs and labels to strings (except None)
+        residue_pdb_ids = [
+            str(residue_pdb_id) if residue_pdb_id else residue_pdb_id
+            for residue_pdb_id in residue_pdb_ids
+        ]
+        residue_labels = [
+            str(residue_label) if residue_label else residue_label
+            for residue_label in residue_labels
+        ]
 
         return residue_pdb_ids, residue_labels
 
