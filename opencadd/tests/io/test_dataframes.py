65cf85c6a8a19b8c2193d1989088c937984dc1f4
Unknown <dominique.sydow@posteo.de>
2020-09-25 15:23:58 +0200

    Update io tests

--- opencadd/tests/io/test_dataframes.py
+++ opencadd/tests/io/test_dataframes.py
@@ -7,110 +7,173 @@ from pathlib import Path
 import pandas as pd
 import pytest
 
-from opencadd.io.dataframes import Mol2ToDataFrame, PdbToDataFrame
+from opencadd.io import DataFrame
 from opencadd.io.schema import DATAFRAME_COLUMNS
 
 PATH_TEST_DATA = Path(__name__).parent / "opencadd" / "tests" / "data" / "io"
 
 
-def _dataframe_format_tests(dataframe):
+class TestsDataFrame:
     """
-    Default tests: Check if output is a DataFrame, if column names (and their order) and
-    the column dtypes are correct.
-
-    Parameter
-    ---------
-    dataframe : pandas.DataFrame
-        DataFrame with structural data.
+    Tests the DataFrame class methods.
     """
 
-    assert isinstance(dataframe, pd.DataFrame)
-
-    # Create DataFrame for default dataframe columns
-    default_dataframe_columns = pd.DataFrame(
-        DATAFRAME_COLUMNS["default"], columns=["name", "dtype"]
+    @pytest.mark.parametrize(
+        "filepath, verbose",
+        [
+            (PATH_TEST_DATA / "2itz.pdb", False),
+            (PATH_TEST_DATA / "2itz.pdb", True),
+            (PATH_TEST_DATA / "2itz_chainA_protein.mol2", False),
+            (PATH_TEST_DATA / "2itz_protein.mol2", True),
+            (PATH_TEST_DATA / "2itz_protein.mol2", False),
+            (PATH_TEST_DATA / "2itz_protein.mol2", True),
+        ],
     )
-    assert dataframe.columns.to_list() == default_dataframe_columns["name"].to_list()
-    assert dataframe.dtypes.to_list() == default_dataframe_columns["dtype"].to_list()
+    def test_from_file(self, filepath, verbose):
+        """
+        Test if input produces a DataFrame.
+        """
 
+        dataframe = DataFrame.from_file(filepath, verbose)
+        isinstance(dataframe, pd.DataFrame)
 
-@pytest.mark.parametrize("pdb_file", [PATH_TEST_DATA / "2itz.pdb"])
-def test_from_pdb_file(pdb_file):
-    """
-    Test loading pdb files as DataFrame.
+    @pytest.mark.parametrize(
+        "filepath, format, verbose",
+        [
+            (PATH_TEST_DATA / "2itz.pdb", "pdb", False),
+            (PATH_TEST_DATA / "2itz.pdb", "pdb", True),
+            (PATH_TEST_DATA / "2itz_chainA_protein.mol2", "mol2", False),
+            (PATH_TEST_DATA / "2itz_protein.mol2", "mol2", True),
+            (PATH_TEST_DATA / "2itz_protein.mol2", "mol2", False),
+            (PATH_TEST_DATA / "2itz_protein.mol2", "mol2", True),
+        ],
+    )
+    def test_from_text(self, filepath, format, verbose):
+        """
+        Test if input produces a DataFrame.
+        Note: Use file as test function input; file content will be read as string,
+        which is the input for the class method to be tested here!
+        """
 
-    Parameters
-    ----------
-    pdb_file : pathlib.Path or str
-        Path to pdb file.
-    """
+        # Let's load a file's content as string (text) to simulate example input data
+        with open(filepath, "r") as f:
+            text = f.read()
 
-    pdb_to_df = PdbToDataFrame()
-    df = pdb_to_df.from_file(pdb_file)
+        dataframe = DataFrame.from_text(text, format, verbose)
+        isinstance(dataframe, pd.DataFrame)
 
-    _dataframe_format_tests(df)
+    @pytest.mark.parametrize(
+        "filepath, format",
+        [
+            (PATH_TEST_DATA / "2itz.pdb", "mol2"),
+            (PATH_TEST_DATA / "2itz.pdb", "unknown format"),
+            (PATH_TEST_DATA / "2itz_chainA_protein.mol2", "pdb"),
+            (PATH_TEST_DATA / "2itz_chainA_protein.mol2", "unknown format"),
+            (PATH_TEST_DATA / "invalid_data", "mol2"),
+            (PATH_TEST_DATA / "invalid_data", "pdb"),
+            (PATH_TEST_DATA / "invalid_data", "unknown format"),
+        ],
+    )
+    def test_from_text_raises(self, filepath, format):
+        """
+        Test if input produces a ValueError for invalid inputs.
+        Note: Use file as test function input; file content will be read as string,
+        which is the input for the class method to be tested here!
+        """
 
+        # Let's load a file's content as string (text) to simulate example input data
+        with open(filepath, "r") as f:
+            text = f.read()
 
-@pytest.mark.parametrize("pdb_file", [PATH_TEST_DATA / "2itz.pdb"])
-def test_from_pdb_text(pdb_file):
-    """
-    Test loading pdb file contents (text) as DataFrame.
+        with pytest.raises(ValueError):
+            DataFrame.from_text(text, format)
 
-    Parameters
-    ----------
-    pdb_file : pathlib.Path or str
-        Path to pdb file.
-    """
+    @pytest.mark.parametrize("pdb_file", [PATH_TEST_DATA / "2itz.pdb"])
+    def test_from_pdb_file(self, pdb_file):
+        """
+        Test loading pdb files as DataFrame.
 
-    # Let's load a file's content as string (text) to simulate example input data
-    with open(pdb_file, "r") as f:
-        pdb_text = f.read()
+        Parameters
+        ----------
+        pdb_file : pathlib.Path or str
+            Path to pdb file.
+        """
 
-    pdb_to_df = PdbToDataFrame()
-    df = pdb_to_df.from_text(pdb_text)
+        df = DataFrame._from_pdb_file(pdb_file)
+        self._dataframe_format_tests(df)
 
-    _dataframe_format_tests(df)
+    @pytest.mark.parametrize("pdb_file", [PATH_TEST_DATA / "2itz.pdb"])
+    def test_from_pdb_text(self, pdb_file):
+        """
+        Test loading pdb file contents (text) as DataFrame.
 
+        Parameters
+        ----------
+        pdb_file : pathlib.Path or str
+            Path to pdb file.
+        """
 
-@pytest.mark.parametrize(
-    "mol2_file",
-    [PATH_TEST_DATA / "2itz_chainA_protein.mol2", PATH_TEST_DATA / "2itz_protein.mol2"],
-)
-def test_from_mol2_file(mol2_file):
-    """
-    Test loading mol2 files as DataFrame.
+        # Let's load a file's content as string (text) to simulate example input data
+        with open(pdb_file, "r") as f:
+            pdb_text = f.read()
 
-    Parameters
-    ----------
-    mol2_file : pathlib.Path or str
-        Path to mol2 file.
-    """
+        df = DataFrame._from_pdb_text(pdb_text)
+        self._dataframe_format_tests(df)
 
-    mol2_to_df = Mol2ToDataFrame()
-    df = mol2_to_df.from_file(mol2_file)
+    @pytest.mark.parametrize(
+        "mol2_file",
+        [PATH_TEST_DATA / "2itz_chainA_protein.mol2", PATH_TEST_DATA / "2itz_protein.mol2"],
+    )
+    def test_from_mol2_file(self, mol2_file):
+        """
+        Test loading mol2 files as DataFrame.
 
-    _dataframe_format_tests(df)
+        Parameters
+        ----------
+        mol2_file : pathlib.Path or str
+            Path to mol2 file.
+        """
 
+        df = DataFrame._from_mol2_file(mol2_file)
+        self._dataframe_format_tests(df)
 
-@pytest.mark.parametrize(
-    "mol2_file",
-    [PATH_TEST_DATA / "2itz_chainA_protein.mol2", PATH_TEST_DATA / "2itz_protein.mol2"],
-)
-def test_from_mol2_text(mol2_file):
-    """
-    Test loading mol2 file contents (text) as DataFrame.
+    @pytest.mark.parametrize(
+        "mol2_file",
+        [PATH_TEST_DATA / "2itz_chainA_protein.mol2", PATH_TEST_DATA / "2itz_protein.mol2"],
+    )
+    def test_from_mol2_text(self, mol2_file):
+        """
+        Test loading mol2 file contents (text) as DataFrame.
 
-    Parameters
-    ----------
-    mol2_file : pathlib.Path or str
-        Path to mol2 file.
-    """
+        Parameters
+        ----------
+        mol2_file : pathlib.Path or str
+            Path to mol2 file.
+        """
 
-    # Let's load a file's content as string (text) to simulate example input data
-    with open(mol2_file, "r") as f:
-        mol2_text = f.read()
+        # Let's load a file's content as string (text) to simulate example input data
+        with open(mol2_file, "r") as f:
+            mol2_text = f.read()
 
-    mol2_to_df = Mol2ToDataFrame()
-    df = mol2_to_df.from_text(mol2_text)
+        df = DataFrame._from_mol2_text(mol2_text)
+        self._dataframe_format_tests(df)
 
-    _dataframe_format_tests(df)
+    def _dataframe_format_tests(self, dataframe):
+        """
+        Default tests: Check if output is a DataFrame, if column names (and their order) and
+        the column dtypes are correct.
+
+        Parameter
+        ---------
+        dataframe : pandas.DataFrame
+            DataFrame with structural data.
+        """
+
+        assert isinstance(dataframe, pd.DataFrame)
+
+        # Create DataFrame for default dataframe columns
+        default_dataframe_columns = pd.DataFrame(
+            DATAFRAME_COLUMNS["default"], columns=["name", "dtype"]
+        )
+        assert dataframe.columns.to_list() == default_dataframe_columns["name"].to_list()
+        assert dataframe.dtypes.to_list() == default_dataframe_columns["dtype"].to_list()
--- opencadd/tests/io/test_rdkit.py
+++ opencadd/tests/io/test_rdkit.py
@@ -7,61 +7,96 @@ from pathlib import Path
 import pytest
 from rdkit import Chem
 
-from opencadd.io.rdkit import Mol2ToRdkitMol
+from opencadd.io import Rdkit
 
 PATH_TEST_DATA = Path(__name__).parent / "opencadd" / "tests" / "data" / "io"
 
 
-def _rdkit_format_tests(rmol):
+class TestsRdkitMol:
     """
-    Default tests: Check if output is an RDKit molecule.
-
-    Parameter
-    ---------
-    rmol : Chem.rdchem.Mol
-        RDKit molecule.
+    Tests the RdkitMol class methods.
     """
 
-    assert isinstance(rmol, Chem.rdchem.Mol)
+    @pytest.mark.parametrize(
+        "filepath, compute2d",
+        [
+            (PATH_TEST_DATA / "2itz_chainA_ligand.mol2", True),
+            (PATH_TEST_DATA / "2itz_chainA_ligand.mol2", False),
+        ],
+    )
+    def test_from_file(self, filepath, compute2d):
+        """
+        Test if input produces an RDKit molecule.
+        """
 
+        rmol = Rdkit.from_file(filepath, compute2d)
+        self._rdkit_format_tests(rmol)
 
-@pytest.mark.parametrize(
-    "mol2_file", [PATH_TEST_DATA / "2itz_chainA_ligand.mol2"],
-)
-def test_from_mol2_file(mol2_file):
-    """
-    Test loading mol2 files as RDKit molecule.
+    @pytest.mark.parametrize(
+        "filepath, format, compute2d",
+        [
+            (PATH_TEST_DATA / "2itz_chainA_ligand.mol2", "mol2", True),
+            (PATH_TEST_DATA / "2itz_chainA_ligand.mol2", "mol2", False),
+        ],
+    )
+    def test_from_text(self, filepath, format, compute2d):
+        """
+        Test if input produces an RDKit molecule.
+        Note: Use file as test function input; file content will be read as string,
+        which is the input for the class method to be tested here!
+        """
 
-    Parameters
-    ----------
-    mol2_file : pathlib.Path or str
-        Path to mol2 file.
-    """
+        # Let's load a file's content as string (text) to simulate example input data
+        with open(filepath, "r") as f:
+            text = f.read()
 
-    mol2_to_rmol = Mol2ToRdkitMol()
-    rmol = mol2_to_rmol.from_file(mol2_file)
+        rmol = Rdkit.from_text(text, format, compute2d)
+        self._rdkit_format_tests(rmol)
 
-    _rdkit_format_tests(rmol)
+    @pytest.mark.parametrize(
+        "mol2_file", [PATH_TEST_DATA / "2itz_chainA_ligand.mol2"],
+    )
+    def test_from_mol2_file(self, mol2_file):
+        """
+        Test loading mol2 files as RDKit molecule.
 
+        Parameters
+        ----------
+        mol2_file : pathlib.Path or str
+            Path to mol2 file.
+        """
 
-@pytest.mark.parametrize(
-    "mol2_file", [PATH_TEST_DATA / "2itz_chainA_ligand.mol2"],
-)
-def test_from_mol2_text(mol2_file):
-    """
-    Test loading mol2 file contents (text) as RDKit molecule.
+        rmol = Rdkit._from_mol2_file(mol2_file)
+        self._rdkit_format_tests(rmol)
 
-    Parameters
-    ----------
-    mol2_file : pathlib.Path or str
-        Path to mol2 file.
-    """
+    @pytest.mark.parametrize(
+        "mol2_file", [PATH_TEST_DATA / "2itz_chainA_ligand.mol2"],
+    )
+    def test_from_mol2_text(self, mol2_file):
+        """
+        Test loading mol2 file contents (text) as RDKit molecule.
 
-    # Let's load a file's content as string (text) to simulate example input data
-    with open(mol2_file, "r") as f:
-        mol2_text = f.read()
+        Parameters
+        ----------
+        mol2_file : pathlib.Path or str
+            Path to mol2 file.
+        """
 
-    mol2_to_rmol = Mol2ToRdkitMol()
-    rmol = mol2_to_rmol.from_text(mol2_text)
+        # Let's load a file's content as string (text) to simulate example input data
+        with open(mol2_file, "r") as f:
+            mol2_text = f.read()
 
-    _rdkit_format_tests(rmol)
+        rmol = Rdkit._from_mol2_text(mol2_text)
+        self._rdkit_format_tests(rmol)
+
+    def _rdkit_format_tests(self, rmol):
+        """
+        Default tests: Check if output is an RDKit molecule.
+
+        Parameter
+        ---------
+        rmol : Chem.rdchem.Mol
+            RDKit molecule.
+        """
+
+        assert isinstance(rmol, Chem.rdchem.Mol)
