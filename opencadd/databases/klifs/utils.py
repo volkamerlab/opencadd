6185ce137592646d2c871dacb78da3540439da96
Unknown <dominique.sydow@posteo.de>
2020-11-21 17:06:58 +0100

    Update KLIFS swagger URL (dev version)

--- opencadd/databases/klifs/utils.py
+++ opencadd/databases/klifs/utils.py
@@ -13,7 +13,7 @@ from bravado.client import SwaggerClient
 
 _logger = logging.getLogger(__name__)
 
-KLIFS_API_DEFINITIONS = "https://klifs.net/swagger/swagger.json"
+KLIFS_API_DEFINITIONS = "https://dev.klifs.net/swagger_v2/swagger.json"
 KLIFS_CLIENT = SwaggerClient.from_url(KLIFS_API_DEFINITIONS, config={"validate_responses": False})
 
 PATH_DATA = Path(__file__).parent / ".." / ".." / "data"
