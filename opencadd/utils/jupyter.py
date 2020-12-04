83fac0eca3b0d668ad09b30f90b0fed7f7828864
Jaime Rodríguez-Guerra <jaimergp@users.noreply.github.com>
2020-12-04 13:20:15 +0100

    Add alert box and details

--- opencadd/utils/jupyter.py
+++ opencadd/utils/jupyter.py
@@ -5,21 +5,50 @@ Utilities helpful in the context of Jupyter Notebooks
 from IPython.display import display, HTML, Markdown
 
 
-def print_html(*args, **kwargs):
+def print_html(*args, sep=" ", **kwargs):
     """
     Displays rich HTML output
+
+    Parameters
+    ----------
+    args : str
+        HTML text to be rendered
+    sep : str, optional=" "
+        Default separator between items in `args`
     """
-    return display(HTML(*args, **kwargs))
+    contents = sep.join([str(a) for a in args])
+    return display(HTML(contents, **kwargs))
 
 
 hprint = print_html
 
 
-def print_markdown(*args, **kwargs):
+def print_markdown(*args, sep=" ", alert=None, hidden=None, **kwargs):
     """
     Displays rich Markdown output
+
+    Parameters
+    ----------
+    args : str
+        Markdown-formatted text to be rendered
+    sep : str, optional=" "
+        Default separator between items in `args`
+    alert : str, optional=None
+        Colored box style. Must be one of info, warning, success or danger.
+    hidden : str, optional=None
+        If set, use value as the title of a collapsed box that can be revealed
+        on click.
     """
-    return display(Markdown(*args, **kwargs))
+    contents = sep.join([str(a) for a in args])
+    if alert is not None:
+        contents = "\n\n".join(
+            [f'<div class="alert alert-block alert-{alert}">', contents, "</div>"]
+        )
+    if hidden is not None:
+        contents = "\n\n".join(
+            [f"<details>", f"<summary>{hidden}</summary>", contents, "</details>"]
+        )
+    return display(Markdown(contents, **kwargs))
 
 
 mprint = print_markdown
