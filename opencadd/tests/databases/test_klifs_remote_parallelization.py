11526c8134094ddc56dd921928f976c735a227aa
Unknown <dominique.sydow@posteo.de>
2021-09-16 09:04:29 +0200

    Satisfy pytest

--- opencadd/tests/databases/test_klifs_remote_parallelization.py
+++ opencadd/tests/databases/test_klifs_remote_parallelization.py
@@ -12,6 +12,7 @@ from opencadd.databases.klifs import setup_remote
 # Set local session
 REMOTE = setup_remote()
 
+
 @pytest.mark.parametrize(
     "klifs_session, structure_klifs_ids, n_cores",
     [(REMOTE, [111, 113], 2)],
@@ -19,17 +20,16 @@ REMOTE = setup_remote()
 def test_remote_parallelization(klifs_session, structure_klifs_ids, n_cores):
 
     pool = Pool(processes=n_cores)
-    
-    pool.starmap(
-        _parallelize_klifs_session, zip(structure_klifs_ids, repeat(klifs_session))
-    )
+
+    pool.starmap(_parallelize_klifs_session, zip(structure_klifs_ids, repeat(klifs_session)))
     pool.close()
     pool.join()
+
 
 def _parallelize_klifs_session(structure_klifs_id, klifs_session):
     """
     Dummy function that simulates a function that takes the KLIFS session as input;
-    shall be used in our test where we want to pass a single klifs_session to multiple functions 
+    shall be used in our test where we want to pass a single klifs_session to multiple functions
     that are called in a parallelized process.
     """
     return klifs_session.structures.by_structure_klifs_id(structure_klifs_id)
