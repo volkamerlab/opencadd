f4977823d0530eb05ce1219db54efbc87681cc81
pipaj97 <61475289+pipaj97@users.noreply.github.com>
2021-12-12 19:21:57 +0100

    push changes

---
+++ opencadd/structure/superposition/benchmark.py
@@ -0,0 +1,4 @@
+import pandas
+
+
+with open 
---
+++ opencadd/tests/structure/test_cli.py
@@ -0,0 +1,8 @@
+"""
+Tests for opencadd.structure.superposition.cli
+"""
+
+import pytest
+
+from opencadd.structure.core import Structure
+from opencadd.structure.superposition.engines.theseus import TheseusAligner
