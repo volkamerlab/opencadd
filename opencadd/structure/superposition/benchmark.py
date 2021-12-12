200074e2054b702ed143ae384615c472aadbdd98
pipaj97 <61475289+pipaj97@users.noreply.github.com>
2021-12-12 19:36:13 +0100

    Revert "push changes"
    This reverts commit f4977823d0530eb05ce1219db54efbc87681cc81.

--- opencadd/structure/superposition/benchmark.py
+++
@@ -1,4 +0,0 @@
-import pandas
-
-
-with open 
--- opencadd/tests/structure/test_cli.py
+++
@@ -1,8 +0,0 @@
-"""
-Tests for opencadd.structure.superposition.cli
-"""
-
-import pytest
-
-from opencadd.structure.core import Structure
-from opencadd.structure.superposition.engines.theseus import TheseusAligner
