120eedb7402bb14e89cac373510340398018ce5c
Armin Ariamajd <armin.ariam@fu-berlin.de>
2022-11-11 19:59:12 +0100

    add pymol module for extracting atom properties within radius

--- opencadd/visualization/pymol_api.py
+++ opencadd/visualization/pymol_api.py
@@ -4,79 +4,74 @@ Functions and routines for easier communication with PyMOL.
 
 
 # Standard library
+from typing import Sequence, Tuple
 from pathlib import Path
 # 3rd party
 from pymol import cmd
 import numpy as np
 
 
-# scans each grid point for pseudoatoms inside HBond distance (input parameter)
-def computeHBondEnvironment(
-        coordinates: np.ndarray,
-        hbond_len: float,
-        filepath_pdb: Path,
+def get_target_atoms_within_radius(
+        filepath_target: Path,
+        coords_center: Sequence[float, float, float],
+        radius: float,
+        properties: Sequence[str] = ("name", "resn"),
+        reinitialize: bool = False,
 ):
     """
-    Calculate # HBond donors and acceptors per grid points within %f A" % (hbond_len)
+    Get the properties for all atoms in a target structure that are within a certain radius of a
+    given point.
 
     Parameters
     ----------
-    coordinates
-    hbond_len
-    filepath_pdb
+    filepath_target : pathlib.Path
+        Path to the target structure file.
+    coords_center : Sequence[float, float, float]
+        Coordinates (x, y, z) of the center of a sphere, to select all target atoms within.
+    radius : float
+        Radius of the sphere.
+    properties : Sequence[str]
+        A sequence of strings corresponding to the atom-property names defined in PyMol.
+        For example, 'name' is the name of the atom, in PyMol notation, and `resn` is the name
+        of the amino acid residue, to which that atom belongs.
+    reinitialize : bool
+        If True, PyMol will be reinitialized once before, and once after running this function,
+        otherwise, no reinitialization will take place. Without reinitialization, everything
+        done in PyMol during this process will remain as the active session, meaning future
+        function calls to this or other PyMol functions will modify the same PyMol session,
+        if those future calls also have this parameter set to False. Depending on your routine,
+        both options can come in handy.
 
     Returns
     -------
-
+    numpy.ndarray
+        A 2-dimensional array of shape (n, k), where 'n' is equal to the number of found atoms,
+        and 'k' is equal to the number of queried properties.
+        Thus, the element at index (i, j) corresponds to the property at index 'j' in
+        `properties`, for the 'i'th atom found by PyMol.
     """
 
-    # variable names flipped, donor-acceptor ist Ansichtssache je nach von Protein oder Ligand
-    # betrachtet
-    # First element is list of acceptor atoms, and second element is list of donors
-    acceptor_donor_atoms = {
-        "ALA": [["N"], ["O"]],
-        "ARG": [["NE", "NH1", "NH2", "N"], ["O"]],
-        "ASN": [["NH2", "N"], ["OD1", "O"]],
-        "ASP": [["OD2", "N"], ["OD1", "OD2", "O"]],
-        "CYS": [["N"], ["O"]],
-        "GLU": [["OE1", "N"], ["OE1", "OE2", "O"]],
-        "GLN": [["NE2", "N"], ["OE1", "O"]],
-        "GLY": [["N"], ["O"]],
-        "HIS": [["ND1", "N"], ["NE2", "O"]],
-        "ILE": [["N"], ["O"]],
-        "LEU": [["N"], ["O"]],
-        "LYS": [["NZ", "N"], ["O"]],
-        "MET": [["N"], ["O"]],
-        "PHE": [["N"], ["O"]],
-        "PRO": [["N"], ["O"]],
-        "SER": [["OG", "N"], ["OG", "O"]],
-        "THR": [["OG1", "N"], ["OG1", "O"]],
-        "TRP": [["NE1", "N"], ["O"]],
-        "TYR": [["OH", "N"], ["OH", "O"]],
-        "VAL": [["N"], ["O"]],
-        "HOH": [["O"], ["H"]],
-        "ZN": [["ZN"], ["ZN"]],
-    }
-
-    cmd.reinitialize()
-    cmd.load(filepath_pdb, object="proteinfile")
-    donors_count = np.zeros(coordinates.shape[0], dtype=np.uint8)
-    acceptors_count = np.zeros(coordinates.shape[0], dtype=np.uint8)
-    for idx, coord in enumerate(coordinates):
-        cmd.pseudoatom("tmpPoint", pos=coord)
-        # radius of circle for energy density
-        cmd.select("proteinAtomshydrophil", f"(tmpPoint around {hbond_len}) and proteinfile")
-        atoms_data = {"stored_names": []}
-        cmd.iterate_state(
-            1, "proteinAtomshydrophil", "stored_names.append([resn, name])", space=atoms_data
-        )
-        cmd.delete("tmpPoint")
-        cmd.delete("proteinAtomshydrophil")
-        atoms_data = atoms_data["stored_names"]
-        # calculates number of donor/acceptor atoms in neighbourhood
-        for res_name, atom_type in atoms_data:
-            if atom_type in acceptor_donor_atoms[res_name][0]:
-                acceptors_count[idx] += 1
-            if atom_type in acceptor_donor_atoms[res_name][1]:
-                donors_count[idx] += 1
-    return acceptors_count, donors_count
+    # TODO: This can most likely be done much faster if calculated internally using distance
+    #  matrices between selected points and protein atoms.
+    if reinitialize:
+        cmd.reinitialize()
+    # Load target structure from file.
+    cmd.load(object="target_structure", filename=filepath_target)
+    # Create pseudo-atom at the given center position.
+    cmd.pseudoatom(object="center", pos=coords_center)
+    # Select all atoms in target that are within the given radius to the center position.
+    cmd.select(
+        name="target_atoms_within_radius",
+        selection=f"(center around {radius}) and target_structure",
+    )
+    # Iterate over selected atoms in PyMol, and extract values of given properties into a list.
+    # TODO: define an internal convention to encode PyMol output strings into ints that can be
+    #  optimally stored and accessed by numpy.
+    selected_atoms_data = []
+    cmd.iterate_state(
+        state=1, # TODO: why 1? is it always 1?
+        selection="target_atoms_within_radius",
+        expression=f"selected_atoms_data.append([{', '.join(properties)}])",
+        space={"selected_atoms_data": selected_atoms_data}
+    )
+    return np.array(selected_atoms_data)
