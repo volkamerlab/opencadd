132f673b25708825e134b0666aa6670fb0f7fbe5
Armin Ariamajd <armin.ariam@fu-berlin.de>
2022-11-13 17:12:41 +0100

    add function to convert pdb to pdbqt using openbabel

--- opencadd/io/pdbqt.py
+++ opencadd/io/pdbqt.py
@@ -1,9 +1,11 @@
 import os
-from typing import NoReturn
+from typing import NoReturn, Tuple, Optional, Union
 from pathlib import Path
 
+from openbabel import pybel
 
-def pdb_to_pdbqt(
+
+def pdb_to_pdbqt_autodocktools(
         filepath_input_pdb: Path,
         filepath_output_pdbqt: Path,
         filepath_mgl_python: Path = "/home/michele/Software/MGLTools-1.5.6/bin/pythonsh",
@@ -37,3 +39,46 @@ def pdb_to_pdbqt(
         f"-r {filepath_input_pdb} -o {filepath_output_pdbqt.with_suffix}"
     )
     return
+
+
+def pdb_to_pdbqt_openbabel(
+        pdb_filepath: Path,
+        pdbqt_filepath: Optional[Path] = None,
+        add_hydrogens: bool = True,
+        protonate_for_pH: Union[float, None] = 7.4,
+        calculate_partial_charges: bool = True,
+):
+    """
+    Convert a PDB file to a PDBQT file, and save it in the given filepath.
+
+    Parameters
+    ----------
+    pdb_filepath: str or pathlib.Path
+        Path to input PDB file.
+    pdbqt_filepath: str or pathlib.Path
+        Path to output PDBQT file.
+    add_hydrogens : bool, Optional, default: True
+        Whether to add hydrogen atoms to the structure.
+    protonate_for_pH : float | None, Optional, default: 7.4
+        pH value to optimize protonation state of the structure. Disabled if `None`.
+    calculate_partial_charges : bool, Optional, default: True
+        Whether to calculate partial charges for each atom.
+
+    Returns
+    -------
+    openbabel.pybel.Molecule
+        Molecule object of PDB file, modified according to the input.
+        The PDBQT file will be stored in the provided path.
+    """
+    # pybel.readfile() provides an iterator over the Molecules in a file.
+    # To access the first (and possibly only) molecule in a file, we use next()
+    molecule = next(pybel.readfile("pdb", str(Path(pdb_filepath).with_suffix(".pdb"))))
+    if protonate_for_pH:
+        molecule.OBMol.CorrectForPH(protonate_for_pH)
+    if add_hydrogens:
+        molecule.addh()
+    if calculate_partial_charges:
+        for atom in molecule.atoms:
+            atom.OBAtom.GetPartialCharge()
+    molecule.write("pdbqt", str(pdbqt_filepath.with_suffix(".pdbqt")), overwrite=True)
+    return molecule
