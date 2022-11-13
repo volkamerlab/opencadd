0ce8380072bd8f4f667d36d075ef1d9ad9b2a68f
Armin Ariamajd <armin.ariam@fu-berlin.de>
2022-11-13 23:17:53 +0100

    add function to parse PDBQT file into pandas DataFrame

--- opencadd/io/pdbqt.py
+++ opencadd/io/pdbqt.py
@@ -1,8 +1,17 @@
+"""
+Functions for handling pdbqt files.
+"""
+
+# Standard library
 import os
 from typing import NoReturn, Tuple, Optional, Union
 from pathlib import Path
-
+# 3rd-party
+import numpy as np
+import pandas as pd
 from openbabel import pybel
+# Self
+from opencadd.misc.parsing import extract_column_from_string_array
 
 
 def pdb_to_pdbqt_autodocktools(
@@ -105,3 +114,68 @@ def extract_autodock_atom_types_from_pdbqt(filepath_pdbqt: Path) -> Tuple[str]:
         lines = f.readlines()
     atom_types = [line.split()[-1] for line in lines if line.startswith("ATOM")]
     return tuple(set(atom_types))
+
+
+def parse_pdbqt(filepath_pdbqt: Path):
+    """
+    Parse a PDBQT file.
+
+    Parameters
+    ----------
+    filepath_pdbqt : pathlib.Path
+        Path to the PDBQT file.
+
+    Returns
+    -------
+    dict(str, pandas.DataFrame)
+        A dictionary of record names (e.g. "ATOM") and their corresponding dataframes.
+
+    References
+    ----------
+    PDB file format documentation:
+        https://ftp.wwpdb.org/pub/pdb/doc/format_descriptions/Format_v33_A4.pdf
+    """
+
+    def parse_atom_records(lines_atom: np.ndarray):
+        """
+        Parse ATOM records
+        """
+        columns = {
+            "serial": ((7, 11), int),
+            "name": ((13, 16), (str, 4)),
+            "altLoc": ((17, 17), (str, 1)),
+            "resName": ((18, 20), (str, 3)),
+            "chainID": ((22, 22), (str, 1)),
+            "resSeq": ((23, 26), int),
+            "iCode": ((27, 27), (str, 1)),
+            "x": ((31, 38), float),
+            "y": ((39, 46), float),
+            "z": ((47, 54), float),
+            "occupancy": ((55, 60), float),
+            "tempFactor": ((61, 66), float),
+            "partial_charge": ((67, 76), float),
+            "autodock_atom_type": ((78, 79), (str, 2))
+        }
+
+        df = pd.DataFrame()
+        for col_name, (col_range, col_dtype) in columns.items():
+            df[col_name] = np.char.strip(
+                extract_column_from_string_array(
+                    array=lines_atom,
+                    char_range=(col_range[0] - 1, col_range[1])
+                )
+            ).astype(col_dtype)
+        return df
+
+    record_parsers = {
+        "ATOM" : parse_atom_records
+    }
+
+    with open(filepath_pdbqt, "r") as f:
+        lines = np.array(f.readlines())
+
+    records = dict()
+    for record, parser in record_parsers.items():
+        record_mask = np.char.startswith(lines, prefix=record)
+        records[record] = parser(lines[record_mask])
+    return records
