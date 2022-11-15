4371ea5315df512029c1dec4a2ab77a0eae0496f
Armin Ariamajd <armin.ariam@fu-berlin.de>
2022-11-15 20:26:49 +0100

    merge method `calculate_energy_grid` into `__init__`

--- opencadd/pipelines/t2fpharm.py
+++ opencadd/pipelines/t2fpharm.py
@@ -26,8 +26,19 @@ class T2FPharm:
             self,
             filepath_target_structure: Union[str, Path],
             path_output: Optional[Union[str, Path]] = None,
+            type_probes: Sequence[TYPE_AUTODOCK_ATOM_TYPE] = ("A", "C", "HD", "OA"),
+            coords_grid_center: Union[Tuple[float, float, float], Literal["auto"]] = "auto",
+            spacing_grid_points: float = 0.6,
+            dims_grid: Optional[Tuple[float, float, float]] = (16, 16, 16),
+            npts: Optional[Tuple[int, int, int]] = None,
     ):
         """
+        Get interaction energies between the target structure and each of the input probe types
+        (plus electrostatic and desolvation energies), calculated using AutoGrid4 at every point
+        on a regular (i.e. evenly spaced) cuboid grid, placed on (usually the binding pocket of)
+        the target structure, as defined by the input arguments.
+
+
         Parameters
         ----------
         filepath_target_structure : str | pathlib.Path
@@ -36,52 +47,8 @@ class T2FPharm:
             An output path to store the results. If not provided, the output files will be
             stored in the same folder as the input file. If a non-existing path is given,
             a new directory will be created with all necessary parent directories.
-        """
-        # Verify the input file is an existing PDBQT file and assign output path.
-        filepath_target = Path(filepath_target_structure)
-        if not filepath_target.is_file():
-            raise ValueError("Input filepath does not point to a file.")
-        try:
-            df_pdbqt = pdbqt.parse_pdbqt(filepath_pdbqt=filepath_target)
-        except Exception as e:
-            raise ValueError("Could not parse the input PDBQT file.") from e
-        if path_output is None:
-            path_output = filepath_target.parent / filepath_target.stem
-        else:
-            path_output = Path(path_output)
-            path_output.mkdir(parents=True, exist_ok=True)
-        # Set instance attributes.
-        self._df_pdbqt: pd.DataFrame = df_pdbqt
-        self._filepath_target: Path = filepath_target
-        self._path_output: Path = path_output
-        # Instance attributes that are set after calling respective methods:
-        self._spacing_grid_points: float = None
-        self._grid: np.ndarray = None
-        self._type_probes: np.ndarray = None
-        self._type_energy_grids: np.ndarray = None
-        self._grid_vacancy: np.ndarray = None
-        self._grid_buriedness = None
-        return
-
-    def calculate_energy_grid(
-            self,
-            type_probes: Sequence[TYPE_AUTODOCK_ATOM_TYPE] = ("A", "C", "HD", "OA"),
-            coords_grid_center: Union[Tuple[float, float, float], Literal["auto"]] = "auto",
-            spacing_grid_points: float = 0.6,
-            dims_grid: Optional[Tuple[float, float, float]] = (16, 16, 16),
-            npts: Optional[Tuple[int, int, int]] = None,
-            return_copy: bool = False,
-    ) -> Optional[np.ndarray]:
-        """
-        Get interaction energies between the target structure and each of the input probe types
-        (plus electrostatic and desolvation energies), calculated using AutoGrid4 at every point
-        on a regular (i.e. evenly spaced) cuboid grid, placed on (usually the binding pocket of) 
-        the target structure, as defined by the input arguments.
-
-        Parameters
-        ----------
         type_probes : Sequence[Literal]
-            AutoDock-defined atom types (e.g. "A", "C", "HD", "N", "NA", "OA", "SA", "Cl") of 
+            AutoDock-defined atom types (e.g. "A", "C", "HD", "N", "NA", "OA", "SA", "Cl") of
             the probes, for which interaction energies must be calculated.
             For a full list of atom types, see `opencadd.io.pdbqt.DATA_AUTODOCK_ATOM_TYPE`.
         coords_grid_center : tuple[float, float, float] | "auto"
@@ -104,39 +71,41 @@ class T2FPharm:
             result in a grid that is slightly larger than the input dimensions (for more info,
             see the function `opencadd.docking.autogrid.calculate_npts`). Therefore, if this
             argument is provided, the `dims_grid` argument will be ignored.
-        return_copy : bool, Optional, default: False
-            Whether to also return a copy of the calculated grid values (when True),
-            or only store internally (when False).
-
-        Returns
-        -------
-        grid_energy : numpy.ndarray, Optional, default: None
-            A 4-dimensional array, where the first three dimensions represent the grid points,
-            and the last dimension contains the data for each grid point. It is the direct
-            return value of the function `opencadd.docking.autogrid.routine_run_autogrid`;
-            for more information, see the documentation of that function.
-
-            Only when the input parameter `return_copy` is set to True, a copy of the calculated
-            grid energies will be returned, otherwise the grid is stored internally for
-            easier further processing, and `None` is returned.
-
         """
+        # Verify the input file is an existing PDBQT file and assign output path.
+        filepath_target = Path(filepath_target_structure)
+        if not filepath_target.is_file():
+            raise ValueError("Input filepath does not point to a file.")
+        try:
+            df_pdbqt = pdbqt.parse_pdbqt(filepath_pdbqt=filepath_target)["ATOM"]
+        except Exception as e:
+            raise ValueError("Could not parse the input PDBQT file.") from e
+        if path_output is None:
+            path_output = filepath_target.parent / filepath_target.stem
+        else:
+            path_output = Path(path_output)
+            path_output.mkdir(parents=True, exist_ok=True)
+        # Verify that all input probe types are valid AutoDock atom types
         type_probes = np.array(type_probes)
         probe_is_invalid = np.isin(
-                    element=type_probes,
-                    test_elements=DATA_AUTODOCK_ATOM_TYPE.type.values,
-                    invert=True
+            element=type_probes,
+            test_elements=DATA_AUTODOCK_ATOM_TYPE.type.values,
+            invert=True
         )
         if np.any(probe_is_invalid):
             raise ValueError(
                 f"The following `type_probes` are not valid AutoDock atom types: "
                 f"{type_probes[probe_is_invalid]}"
             )
-
-        self._spacing_grid_points = spacing_grid_points
-        self._type_probes = type_probes
-        self._type_energy_grids = np.concatenate([type_probes, ["e", "d"]])
-        self._grid = autogrid.routine_run_autogrid(
+        # Set instance attributes.
+        self._filepath_target: Path = filepath_target
+        self._path_output: Path = path_output
+        self._type_probes: np.ndarray = type_probes
+        self._spacing_grid_points: float = spacing_grid_points
+        self._df_pdbqt: pd.DataFrame = df_pdbqt
+        self._type_energy_grids: np.ndarray = np.concatenate([type_probes, ["e", "d"]])
+        # Calculate grid energies using AutoGrid4
+        self._grid: np.ndarray = autogrid.routine_run_autogrid(
             receptor=self._filepath_target,
             gridcenter=coords_grid_center,
             npts=npts if npts is not None else autogrid.calculate_npts(
@@ -147,7 +116,22 @@ class T2FPharm:
             ligand_types=type_probes,
             path_output=self._path_output / self._filepath_target.stem
         )
-        return self._grid.copy() if return_copy else None
+        return
+
+    @property
+    def grid(self):
+        """
+        The complete grid.
+
+        Returns
+        -------
+        numpy.ndarray
+        A 4-dimensional array, where the first three dimensions represent the grid points,
+        and the last dimension contains the data for each grid point. It is the direct
+        return value of the function `opencadd.docking.autogrid.routine_run_autogrid`;
+        for more information, see the documentation of that function.
+        """
+        return self._grid
 
     def calculate_vacancy_from_energy(
             self,
@@ -159,8 +143,6 @@ class T2FPharm:
         """
         Calculate whether each grid point is vacant (or occupied by a target atom), based on given
         reference energy types, and a cutoff value.
-        Notice that before using this method, an energy grid must be calculated using the method
-        `calculate_energy_grid`.
 
         Parameters
         ----------
