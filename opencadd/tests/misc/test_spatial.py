978ab2a7752b6bf1f11a23b509cbdb1e5e5c9b78
Armin Ariamajd <armin.ariam@fu-berlin.de>
2022-11-16 01:23:29 +0100

    add test function for `misc.spatial.grid_distance`

---
+++ opencadd/tests/misc/test_spatial.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+from opencadd.misc import spatial
+
+
+def test_grid_distance():
+    grid = np.zeros((3, 3, 3))
+    grid[1, 1, 1] = 1
+    dists = spatial.grid_distance(
+        grid,
+        directions=spatial.GRID_DIRS,
+        start_indices=np.argwhere(grid),
+        target_value=0,
+        max_dist_in_dir=np.ones(spatial.GRID_DIRS.shape[0])
+    )
+    assert np.array_equal(dists, np.ones(shape=(1, 26)))
