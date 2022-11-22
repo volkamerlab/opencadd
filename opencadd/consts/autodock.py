db5792dcadd46b2d6fbd38721e31efbef2dfe21c
Armin Ariamajd <armin.ariam@fu-berlin.de>
2022-11-22 05:19:51 +0100

    rename variables for more clarity

--- opencadd/consts/autodock.py
+++ opencadd/consts/autodock.py
@@ -1,7 +1,7 @@
 from typing import Literal, NamedTuple
 
 
-class AutoDockAtomType(NamedTuple):
+class AtomType(NamedTuple):
     """
     AutoDock-defined atom type with specified hydrogen-bonding properties.
 
@@ -11,11 +11,11 @@ class AutoDockAtomType(NamedTuple):
         Name of the atom type according to AutoDock, e.g. H, HD, N, O, OA etc.
     description : str
         Short description of the atom type: element name, H-bonding role and type.
-    h_bond_status : Literal[-1, 0, 1], Optional, default = 0
+    hbond_status : Literal[-1, 0, 1], Optional, default = 0
         +1 for H-bond acceptors (i.e. electron-pair donors),
         -1 for H-bond donors (i.e. electron-pair acceptors),
         0 for non H-bonding atom types.
-    H_bond_count : int, Optional, default: 0
+    hbond_count : int, Optional, default: 0
         Number of possible H-bonds for directionally H-bonding atoms, or -1 for spherically
         H-bonding atoms. For all non H-bonding atoms this must be set to 0 (default).
 
@@ -25,11 +25,11 @@ class AutoDockAtomType(NamedTuple):
     """
     name: str
     description: str
-    h_bond_status: Literal[-1, 0, 1] = 0
-    h_bond_count: int = 0
+    hbond_status: Literal[-1, 0, 1] = 0
+    hbond_count: int = 0
 
 
-class AutoDockAtomTypes(NamedTuple):
+class AtomTypes(NamedTuple):
     """
     Collection of AutoDock-defined atom types and their properties,
     used in AutoDock software (e.g. AutoGrid4) and file formats (e.g. PDBQT).
@@ -44,25 +44,25 @@ class AutoDockAtomTypes(NamedTuple):
     https://mmb.irbbarcelona.org/gitlab/BioExcel/structureChecking/blob/5f07d82dc36d1f43733ae3b1ecd9f40aebe8b0a2/biobb_structure_checking/dat/autodock_atomtypes.dat
     """
 
-    H: AutoDockAtomType = AutoDockAtomType("H", "Hydrogen, non H-bonding")
-    H_D1: AutoDockAtomType = AutoDockAtomType("HD", "Hydrogen, H-bond donor, 1 bond", -1, 1)
-    H_S: AutoDockAtomType = AutoDockAtomType("HS", "Hydrogen, H-bond donor, spherical", -1, -1)
-    C_aliph: AutoDockAtomType = AutoDockAtomType("C", "Carbon, aliphatic, non H-bonding")
-    C_arom: AutoDockAtomType = AutoDockAtomType("A", "Carbon, aromatic, non H-bonding")
-    N: AutoDockAtomType = AutoDockAtomType("N", "Nitrogen, non H-bonding")
-    NA: AutoDockAtomType = AutoDockAtomType("NA", "Nitrogen, H-bond acceptor, 1 bond", 1, 1)
-    NS: AutoDockAtomType = AutoDockAtomType("NS", "Nitrogen, H-bond acceptor, spherical", 1, -1)
-    OA: AutoDockAtomType = AutoDockAtomType("OA", "Oxygen, H-bond acceptor, 2 bonds", 1, 2)
-    OS: AutoDockAtomType = AutoDockAtomType("OS", "Oxygen, H-bond acceptor, spherical", 1, -1)
-    F: AutoDockAtomType = AutoDockAtomType("F", "Fluorine, non H-bonding")
-    Mg: AutoDockAtomType = AutoDockAtomType("Mg", "Magnesium, non H-bonding")
-    P: AutoDockAtomType = AutoDockAtomType("P", "Phosphorus, non H-bonding")
-    SA: AutoDockAtomType = AutoDockAtomType("SA", "Sulfur, H-bond acceptor, 2 bonds", 1, 2)
-    S: AutoDockAtomType = AutoDockAtomType("S", "Sulfur, non H-bonding")
-    Cl: AutoDockAtomType = AutoDockAtomType("CL", "Chlorine, non H-bonding")
-    Ca: AutoDockAtomType = AutoDockAtomType("CA", "Calcium, non H-bonding")
-    Mn: AutoDockAtomType = AutoDockAtomType("Mn", "Manganese, non H-bonding")
-    Fe: AutoDockAtomType = AutoDockAtomType("Fe", "Iron, non H-bonding")
-    Zn: AutoDockAtomType = AutoDockAtomType("Zn", "Zinc, non H-bonding")
-    Br: AutoDockAtomType = AutoDockAtomType("Br", "Bromine, non H-bonding")
-    I: AutoDockAtomType = AutoDockAtomType("I", "Iodine, non H-bonding")
+    H: AtomType = AtomType("H", "Hydrogen, non H-bonding")
+    H_D1: AtomType = AtomType("HD", "Hydrogen, H-bond donor, 1 bond", -1, 1)
+    H_S: AtomType = AtomType("HS", "Hydrogen, H-bond donor, spherical", -1, -1)
+    C_aliph: AtomType = AtomType("C", "Carbon, aliphatic, non H-bonding")
+    C_arom: AtomType = AtomType("A", "Carbon, aromatic, non H-bonding")
+    N: AtomType = AtomType("N", "Nitrogen, non H-bonding")
+    NA: AtomType = AtomType("NA", "Nitrogen, H-bond acceptor, 1 bond", 1, 1)
+    NS: AtomType = AtomType("NS", "Nitrogen, H-bond acceptor, spherical", 1, -1)
+    OA: AtomType = AtomType("OA", "Oxygen, H-bond acceptor, 2 bonds", 1, 2)
+    OS: AtomType = AtomType("OS", "Oxygen, H-bond acceptor, spherical", 1, -1)
+    F: AtomType = AtomType("F", "Fluorine, non H-bonding")
+    Mg: AtomType = AtomType("Mg", "Magnesium, non H-bonding")
+    P: AtomType = AtomType("P", "Phosphorus, non H-bonding")
+    SA: AtomType = AtomType("SA", "Sulfur, H-bond acceptor, 2 bonds", 1, 2)
+    S: AtomType = AtomType("S", "Sulfur, non H-bonding")
+    Cl: AtomType = AtomType("CL", "Chlorine, non H-bonding")
+    Ca: AtomType = AtomType("CA", "Calcium, non H-bonding")
+    Mn: AtomType = AtomType("Mn", "Manganese, non H-bonding")
+    Fe: AtomType = AtomType("Fe", "Iron, non H-bonding")
+    Zn: AtomType = AtomType("Zn", "Zinc, non H-bonding")
+    Br: AtomType = AtomType("Br", "Bromine, non H-bonding")
+    I: AtomType = AtomType("I", "Iodine, non H-bonding")
