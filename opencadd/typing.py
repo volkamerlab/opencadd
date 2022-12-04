77a2791397afd5056b6011ca135ae5aaeee82dbc
Armin Ariamajd <armin.ariam@fu-berlin.de>
2022-12-04 22:16:21 +0100

    add ArrayLike to typing

--- opencadd/typing.py
+++ opencadd/typing.py
@@ -1,4 +1,6 @@
-from typing import TypeAlias, Union
+from typing import TypeAlias, Union, Sequence
 from pathlib import Path
+from numpy import ndarray
 
 PathLike: TypeAlias = Union[str, Path]
+ArrayLike: TypeAlias = Union[Sequence, ndarray]
