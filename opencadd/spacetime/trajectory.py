0a79341007c7ba66a29af7f664c8cd351048ca42
Armin Ariamajd <armin.ariam@fu-berlin.de>
2023-01-02 17:58:56 +0100

    add module for trajectory

---
+++ opencadd/spacetime/trajectory.py
@@ -0,0 +1,18 @@
+from typing import Sequence, Union, List
+
+import numpy as np
+import scipy as sp
+import jax.numpy as jnp
+
+
+from opencadd.typing import ArrayLike
+from opencadd.spacetime.spatial import Spatial
+
+
+class Trajectory:
+    def __init__(self, coordinates: ArrayLike):
+        self._coordinates: jnp.ndarray
+        self._spatials: List[Spatial]
+
+        coordinates_array = jnp.asarray(coordinates)
+        return
