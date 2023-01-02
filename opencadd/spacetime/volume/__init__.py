0020753b45eadde6246f1fb309671da13c84fd19
Armin Ariamajd <armin.ariam@fu-berlin.de>
2023-01-02 18:13:28 +0100

    add init to volume sub-package

---
+++ opencadd/spacetime/volume/__init__.py
@@ -0,0 +1,3 @@
+from opencadd.spacetime.volume import rectangular_cuboid, toxel
+from opencadd.spacetime.volume.rectangular_cuboid import RectangularCuboid
+from opencadd.spacetime.volume.toxel import Toxel
