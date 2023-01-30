78489db8cfa0f227f2376e1815b7fe17f334a99e
Armin Ariamajd <armin.ariam@fu-berlin.de>
2023-01-30 15:45:12 +0100

    change sphinx configuration: add extensions, change template, add extension-settings

--- docs_/source/conf.py
+++ docs_/source/conf.py
@@ -11,11 +11,49 @@ copyright = '2023, Volkamer Lab'
 author = 'Volkamer Lab'
 release = '2.0.0'
 
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+# -- Extensions
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon'
+]
 
+autosummary_generate = True
+
+
+# Napoleon settings (ref: https://www.sphinx-doc.org/en/master/usage/extensions/napoleon.html)
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
+
+# EPUB options
+epub_show_urls = 'footnote'
+
+
+
+# -- Templates
+# Add any paths that contain templates here, relative to this directory (i.e. source).
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -24,5 +62,15 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
 html_static_path = ['_static']
+html_theme = 'pydata_sphinx_theme'
+
+# This is added due to this issue:
+#  https://github.com/pydata/pydata-sphinx-theme/issues/1094#issuecomment-1368264928
+html_theme_options = {
+   "logo": {
+      "image_light": "logo-light.png",
+      "image_dark": "logo-dark.png",
+   }
+}
+
