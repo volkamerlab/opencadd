1b6e86428152da729b4802fbb6e6d34ae16f4aa7
Armin Ariamajd <armin.ariam@fu-berlin.de>
2023-03-01 09:37:24 +0100

    `_decorator.py`: update docstrings

--- opencadd/_decorator.py
+++ opencadd/_decorator.py
@@ -1,5 +1,5 @@
 """
-Custom decorators used by other functions in the package.
+Custom decorators used by different functions within the openCADD library.
 """
 
 from typing import Callable, Optional, Union, Tuple, Type, NamedTuple
@@ -7,21 +7,25 @@ from functools import wraps
 import time
 
 
+__author__ = "Armin Ariamajd"
+
+
 class RetryConfig(NamedTuple):
     """
-    Configuration for `retry_on_exception` decorator.
+    Configuration for the `retry_on_exception` decorator.
 
     Attributes
     ----------
-    num_tries : int, optional, default: 3
-        Maximum number of times the decorated function will be called before the exception is
-        reraised.
-    sleep_time_init : float, optional, default = 1
+    num_tries : int, default: 3
+        Maximum number of times the decorated function will be called
+        before an exception is reraised.
+    sleep_time_init : float, default: 1
         Amount of time (in seconds) to wait before two function calls.
-    sleep_time_scale : float, optional, default = 3
-        Scaling factor for `sleep_time_init`. After the `n`-th function call, the waiting time
-        will be equal to `sleep_time_init * (sleep_time_scale ** n-1)`. This can be used to
-        scale down/up the waiting time between function calls.
+    sleep_time_scale : float, default: 3
+        Scaling factor for `sleep_time_init`.
+        This can be used to scale down/up the waiting time between function calls.
+        After the n-th function call, the waiting time will be equal to:
+        `sleep_time_init` * `sleep_time_scale` ^ (n - 1).
     """
     num_tries: int = 3
     sleep_time_init: float = 1
@@ -29,42 +33,44 @@ class RetryConfig(NamedTuple):
 
 
 def retry_on_exception(
-    _function: Optional[Callable] = None,
+    function: Optional[Callable] = None,
     *,
     config: RetryConfig = RetryConfig(),
     catch: Union[Type[Exception], Tuple[Type[Exception]]] = Exception,
 ) -> Callable:
     """
     Decorator to retry a function call for a given number of times (while waiting for a certain
-    amount between calls), when one of the given exceptions occurs.
+    amount of time between calls), when one of the given exceptions is raised.
 
     Parameters
     ----------
-    config : opencadd.decorator.RetryConfig, optional, default: RetryConfig(3, 1, 3)
-        Retry configurations.
-    catch : Type[Exception] | Tuple[Type[Exception]], optional, default: Exception
-        Exception type(s) that will be ignored during the retries. All other exceptions will be
-        raised immediately.
+    function : callable
+        The function to be decorated.
+    config : opencadd._decorator.RetryConfig, default: RetryConfig(3, 1, 3)
+        Retry configuration.
+    catch : Type[Exception] | Tuple[Type[Exception]], default: Exception
+        Exception type(s) that will be ignored during the retries. 
+        All other exceptions will be raised immediately.
 
     Returns
     -------
-    Callable
+    callable
         Decorated function.
     """
     if not isinstance(config.num_tries, int) or config.num_tries < 1:
         raise ValueError("`num_tries` must be a positive integer.")
 
-    def retry_decorator(function):
-        @wraps(function)
+    def retry_decorator(func):
+        @wraps(func)
         def retry_wrapper(*args, **kwargs):
             curr_sleep_seconds = config.sleep_time_init
             for try_count in range(config.num_tries):
                 try:
-                    return function(*args, **kwargs)
+                    return func(*args, **kwargs)
                 except catch as e:
                     if try_count == config.num_tries - 1:
                         raise e
                     time.sleep(curr_sleep_seconds)
                     curr_sleep_seconds *= config.sleep_time_scale
         return retry_wrapper
-    return retry_decorator if _function is None else retry_decorator(_function)
+    return retry_decorator if function is None else retry_decorator(function)
